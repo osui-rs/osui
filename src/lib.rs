6c0dde8e85ca9728cfb0f030780dd6b906550276
Leo dev <klesti.selimay@icloud.com>
2025-06-24 17:21:12 +0200

    test

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,17 @@
-use std::{any::TypeId, thread, time::Duration};
+pub mod text;
+pub mod macros;
 
-pub mod output;
+use std::fmt::Debug;
+
+use downcast_rs::{impl_downcast, DowncastSync};
+pub trait Component: DowncastSync + Debug {}
+impl_downcast!(sync Component);
+
+pub trait Element {
+    fn get_component<T: Component + 'static>(&mut self) -> Option<&mut T>;
+    fn add_component<T: Component + 'static>(&mut self, component: T);
+}
 
 pub trait Extension {
-    fn tick(&mut self, args: &mut Args);
-}
-
-pub trait BaseElement {
-    fn init(&mut self);
-    fn get_components<'a>(&'a mut self) -> &'a mut Vec<Box<dyn ElementComponent>>;
-}
-
-pub trait ElementComponent {
-    fn get_type_id(&self) -> TypeId;
-}
-
-pub struct Args {
-    pub tick_rate: u16,
-    pub base_elements: Vec<Box<dyn BaseElement>>,
-}
-
-pub fn run(mut args: Args, mut extensions: Vec<Box<dyn Extension>>) {
-    for elem in &mut args.base_elements {
-        elem.init();
-    }
-
-    loop {
-        for extension in &mut extensions {
-            extension.tick(&mut args);
-        }
-
-        thread::sleep(Duration::from_millis(1000 / args.tick_rate as u64));
-    }
+    fn tick(&mut self);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
 use osui::{
-    output::{OutputExtension, RenderAble},
-    run, Args, BaseElement,
+    text::{text, DisplayComponent},
+    Element,
 };
 
-pub struct MyElem(*mut Vec<Box<dyn osui::ElementComponent>>);
-impl BaseElement for MyElem {
-    fn init(&mut self) {
-        unsafe {
-            (*self.0).push(RenderAble::new(|| String::from("value")));
-        }
-    }
-
-    fn get_components<'a>(&'a mut self) -> &'a mut Vec<Box<dyn osui::ElementComponent>> {
-        unsafe { &mut *self.0 }
-    }
-}
-
-impl MyElem {
-    pub fn new(mut v: Vec<Box<dyn osui::ElementComponent>>) -> Box<MyElem> {
-        Box::new(MyElem(&mut v))
-    }
-}
-
 fn main() {
-    let args = Args {
-        tick_rate: 40,
-        base_elements: vec![],
-    };
-    run(args, vec![Box::new(OutputExtension)]);
+    let mut e = text("Hello, World!");
+
+    if let Some(comp) = e.get_component::<DisplayComponent>() {
+        println!("Ok: {:?}", comp);
+    }
 }
