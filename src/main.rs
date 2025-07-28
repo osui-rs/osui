a79617e2199055c25bc4753107e2a24eeab6ed5c
Leo dev <klesti.selimay@icloud.com>
2025-07-28 15:28:44 +0200

    changed main code

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,16 @@ fn main() -> std::io::Result<()> {
     let screen = Screen::new();
     let count = use_state(0);
 
-    std::thread::spawn({
-        let count = count.clone();
-        move || loop {
-            std::thread::sleep(std::time::Duration::from_millis(50));
-            **count.get() += 1;
-            count.update();
-        }
-    });
-
-    // let mut r = Rsx(Vec::new());
-    // r.create_element(
-    //     {
-    //         let count = count.clone();
-    //         move || WidgetLoad::new(format!("{count}"))
-    //     },
-    //     vec![Box::new(count)],
-    // );
-
-    // r.draw(&screen);
-
     rsx! {
         %count
         "{count}"
     }
     .draw(&screen);
+
+    std::thread::spawn(move || loop {
+        std::thread::sleep(std::time::Duration::from_millis(50));
+        **count.get() += 1;
+    });
 
     screen.run()
 }
